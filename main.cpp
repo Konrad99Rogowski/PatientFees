4c0c792193b505b3e90bc6daa149d9bef6cdaecc
felidp <36744929+felidp@users.noreply.github.com>
2018-02-25 16:42:40 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,15 +8,15 @@ int main(){
     int m;
     
     PatientAccount p1;
-    //get number of days
+    //get number of days w/ data validation
     cin >> d;
     p1.UpdatePatient(d);
     
-    //show surgery list and get number
+    //show surgery list and get number w/ data validation
     cin >> s
     p1.addSurgery(s);
     
-    //show medication list and get number
+    //show medication list and get number w/ data validation
     cin >> m
     p1.addMedication(m);
     cout<<"Total $"<<p1.getCost()<<endl;
