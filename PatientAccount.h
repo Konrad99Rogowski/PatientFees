cd4e55a673339b50a0d61405a8b3eeef4778192c
Konrad Rogowski <konradrogowski1@gmail.com>
2018-02-26 17:02:16 -0500

    Update PatientAccount.h

--- PatientAccount.h
+++ PatientAccount.h
@@ -3,33 +3,33 @@
 using namespace std;
 
 class PatientAccount {
-  private:
-  double cost = 0;
-  int days;
-  const double DAILY_RATE = 200.0;
-  
-  public:
-  PatientAccount(){
-    days = 1;
-    cost = 200.0;
-  }
-  void UpdatePatient(int d){
-    cost = 0;
-    days = d;
-    cost += days * DAILY_RATE;
-  }
-  void stayCost( int dy){
-    days = dy;
-    cout<< "\nhospital stay cost: $" << days * DAILY_RATE<< endl;
-  }
-  void updateCost(double c) {
-    cost += c;
-  }
-  double getCost(){
-    return cost;
-  }
-  int getDays(){
-    return days;
-  }
-  
+private:
+	double cost = 0;
+	int days;
+	const double DAILY_RATE = 200.0;
+
+public:
+	PatientAccount(){
+		days = 1;
+		cost = 200.0;
+	}
+	void UpdatePatient(int d){
+		cost = 0;
+		days = d;
+		cost += days * DAILY_RATE;
+	}
+	void stayCost(int dy){
+		days = dy;
+		cout << "\n Hospital stay cost: $" << days * DAILY_RATE << endl;
+	}
+	void updateCost(double c) {
+		cost += c;
+	}
+	double getCost(){
+		return cost;
+	}
+	int getDays(){
+		return days;
+	}
+
 };
